0b16696393b8e1554d17653b8710544ad445ffba
Kaif Azmi <>
2024-02-10 14:45:14 +0530

    Added two connection locally to chat

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
-
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => { 
-    
+
+io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
 
-    socket.on('from_client', () => {
-        console.log("event coming from client");
+    socket.on('msg_send', (data) => {
+        console.log(data);
+        // io.emit('msg_rcvd', data);
+        // socket.emit('msg_rcvd', data)
+        socket.broadcast.emit('msg_rcvd', data)
     })
 
     setInterval(() => {
@@ -20,7 +22,7 @@ io.on('connection', (socket) => {
 });
 
 app.use('/', express.static(__dirname + '/public'));
-
 server.listen(3000, () => {
     console.log('Server started');
-});
+})
+
